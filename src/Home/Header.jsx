9b1a04d458db6e94a4666df9c17301a35be91722
Alarice S <ran@192-168-1-113.tpgi.com.au>
2020-01-15 02:53:34 +1100

    add package.json

--- src/Home/Header.jsx
+++ src/Home/Header.jsx
@@ -5,8 +5,8 @@ export default function Header(props) {
   return (
     <header {...props} id="header">
       <Link id="logo">
-        <img alt="logo" src="/Asset/logo.svg" />
-        <img alt="Apollo" src="/Asset/apollo.svg" />
+        <img alt="logo" src="./Asset/logo.svg" />
+        <img alt="Apollo" src="./Asset/apollo.svg" />
       </Link>
     </header>
   );
